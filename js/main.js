ae9e469bf5ce4a26b92306b38c87dc138c3a2e60
Joseph T. Obagbemisoye <jtob91@yahoo.com>
2016-12-27 02:37:41 +0100

    Initial commit

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function run(){
         url:'https://news.google.com/?output=rss',   
         dataType: "jsonp",
         //contentType: "text/xml; charset=utf-8",
-        //headers: { "Access-Control-Allow-Origin":"*",},                
+        headers: { "Access-Control-Allow-Origin":"*",},                
 
         success: function(xml) {
         alert("success");
