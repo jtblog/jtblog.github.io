89a5dd7697da28b0069b63857c7a98cddac7b413
Joseph T. Obagbemisoye <jtob91@yahoo.com>
2016-12-27 02:18:15 +0100

    Initial commit

--- js/main.js
+++ js/main.js
@@ -3,5 +3,17 @@ window.onload = function(){
 }  
 
 function run(){
-	alert("");
+	getFEED('http://punchng.com/feed/', function(request){
+	    var response = request.currentTarget.response || request.target.responseText;
+	    alert(JSON.stringify(response));
+	});
+}
+
+function getFEED(url, success) {
+    var xhr = new XMLHttpRequest();
+    if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); // fix IE8/9
+    xhr.open('GET', url);
+    xhr.onload = success;
+    xhr.send();
+    return xhr;
 }
