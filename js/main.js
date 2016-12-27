6cabe2d15dae5ae61a41ec9d6328b77ca6d02991
Joseph T. Obagbemisoye <jtob91@yahoo.com>
2016-12-27 02:34:52 +0100

    Initial commit

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@ window.onload = function(){
 }  
 
 function run(){
-	$('#sidebar').html("Loading");
+	$.ajax({
+        type: "GET",
+        url:'https://news.google.com/?output=rss',   
+        dataType: "jsonp",
+        //contentType: "text/xml; charset=utf-8",
+        //headers: { "Access-Control-Allow-Origin":"*",},                
+
+        success: function(xml) {
+        alert("success");
+        }   
+	});
 }
