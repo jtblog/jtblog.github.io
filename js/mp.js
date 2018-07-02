aee7e4c17cd14b7ff1c8167f2423552c4e0361d1
Joseph T. Obagbemisoye <jtob91@yahoo.com>
2018-07-02 01:50:48 +0100

    Update mp.js

--- js/mp.js
+++ js/mp.js
@@ -20,8 +20,7 @@ function loadimages(bg, obj, en){
   var con = bg;
   if(obj.data.length <= 4){
     for(var i=0; i < obj.data.length; i++){
-      con = con + '<img id = "' + obj.data[i].sha + '" src="' + obj.data[i].download_url + 
-      '" alt="" title="" style="position: relative; left: ' + (i*150) + 'px;"/>';
+      con = con + '<img id = "' + obj.data[i].sha + '" src="' + obj.data[i].download_url + '" alt="" title="" style="position: relative; left: ' + (i*150) + 'px; "/>';
     }
   }
   con = con + en;
