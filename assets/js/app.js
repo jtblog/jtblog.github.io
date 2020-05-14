f2e765e978528ec0244139161a6675c03f01ce6f
Joseph T. Obagbemisoye <jtob91@yahoo.com>
2020-05-14 07:12:00 +0100

    ""

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,27 @@
 const attribution = '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-var lat = 51.505;
-var long = -0.09;
+var lat = 6.4474;
+var long = 3.3903;
 var mymap;
 
 document.addEventListener('DOMContentLoaded', function() {
-    getLocation();
+    var site = window.location.href + "";
+    if(isAvailable("mapid")){
+        getLocation();
+    }
 });
+
+function isAvailable(_id){
+    try{
+        if(document.getElementById("mapid") == null || document.getElementById("mapid") == undefined){
+            return false
+        }else{
+            return true
+        }
+    }catch(e){
+        return false
+    }
+}
 
 function getLocation(){
     if (navigator.geolocation) {
@@ -25,4 +40,12 @@ function showPosition(position) {
     mymap = L.map('mapid').setView([lat, long], 13);
     const tiles = L.tileLayer(tileUrl, { attribution })
     tiles.addTo(mymap);
+    var marker = L.marker([lat, long]).addTo(mymap);
+    
+    var circle = L.circle([lat-0.100, long], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 100
+    }).addTo(mymap);
 }
